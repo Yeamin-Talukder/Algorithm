97da86908540703b0b72000df430143be992380c
YEAMIN <mdyeamen611@gmail.com>
2025-05-10 23:18:18 +0600

    Add Topological Sort

---
+++ Graph/Topological Sort using BFS (Kahn's algorithm) .cpp
@@ -0,0 +1,32 @@
+
+class Topological_BFS
+{
+    public : 
+
+    vector<ll> sorted;
+    queue<ll> zero_inorder;
+    Topological_BFS ( ll n , ll start_node , ll end_node , vector<vector<ll>> &edge , vector<ll> &inorder) // inorder in the number of edge directed to a node
+    {
+        for (ll i = start_node; i <= end_node ; i++) 
+        {
+            if(inorder[i] == 0) zero_inorder.push(i);
+        }
+
+        while(!zero_inorder.empty())
+        {
+            ll node = zero_inorder.front(); 
+            zero_inorder.pop();
+            sorted.push_back(node);
+
+            for(auto child : edge[node]) 
+            {
+                inorder[child]--;
+                if(inorder[child] == 0) zero_inorder.push(child);
+            }
+        }
+
+    }
+
+
+};
+
---
+++ Graph/Topological Sort using DFS.cpp
@@ -0,0 +1,24 @@
+class Topological_DFS
+{
+    public :
+
+    vector<ll> vis,sorted;
+    stack<ll> stk;
+    Topological_DFS(ll n , ll start_node , ll end_node, vector<vector<ll>> &edge)
+    {
+        vis.resize(n+1,0);
+        for (ll i = start_node; i <= end_node; i++) 
+        {
+            if(vis[i] == 0) dfs(i,edge);
+        }
+
+        while(!stk.empty()) {sorted.push_back(stk.top()); stk.pop();}
+    }
+
+    void dfs( ll node , vector<vector<ll>>&edge )
+        {
+            vis[node] = 1;
+            for(auto child : edge[node]) if(vis[child] == 0) dfs(child,edge);
+            stk.push(node);
+        }
+};
