02a5d7fd61513265aa125ec8e29f25719e6fe540
YEAMIN <mdyeamen611@gmail.com>
2025-05-25 04:16:05 +0600

    update this

--- Graph/Finding Bridges  and Articulation point(Tarjan’s Algorithm).cpp
+++ Graph/Finding Bridges  and Articulation point(Tarjan’s Algorithm).cpp
@@ -1,12 +1,14 @@
 
 //Time complexcity : O(V+E)
 
+
 class BRIDGE
 {
     public : 
 
     vector<ll> vis,discover_time,low_time;
     vector<pair<ll,ll>> bridge_edge;
+    set<ll> point;
 
     BRIDGE(ll n , vector<vector<ll>> &edge)
     {
@@ -26,23 +28,30 @@ class BRIDGE
     void dfs(ll node , ll parent , ll &discover_id , vector<vector<ll>> &edge)
     {
         vis[node] = 1;
-        discover_time[node] = discover_id;
-        low_time[node] = discover_id;
+        discover_time[node] = low_time[node] = discover_id++;
 
-        discover_id++;
-
+        ll count = 0;
         for(auto child : edge[node])
         {
             if(child == parent) continue;
 
-            if(vis[child] == 1){ low_time[node] = min(low_time[node],low_time[child]); continue;}
-            else dfs(child , node , discover_id , edge);
+            if(vis[child] == 1)
+            { 
+                low_time[node] = min(low_time[node],discover_time[child]); 
+                continue;
+            }
+            
+            dfs(child , node , discover_id , edge);
+            count++;
             
             low_time[node] = min(low_time[node],low_time[child]);
+
             if(discover_time[child] == low_time[child] ) bridge_edge.push_back({node,child});
+            if(discover_time[node] <= low_time[child] && parent != -1) point.insert(node);
 
         }
 
+        if(parent == -1 && count > 1) {point.insert(node);}
     }
 
 };
